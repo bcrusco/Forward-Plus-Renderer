6e72a1b69032b0aa35464a094240f5c98ae9099a
Bradley Crusco <bcrusco627@gmail.com>
2015-12-01 19:45:31 -0500

    Correct tab.

--- Forward-Plus/Forward-Plus/source/shaders/depth.vert.glsl
+++ Forward-Plus/Forward-Plus/source/shaders/depth.vert.glsl
@@ -7,5 +7,5 @@ uniform mat4 u_view;
 uniform mat4 u_model;
 
 void main() {
-  gl_Position = u_projection * u_view * u_model * vec4(position, 1.0);
+	gl_Position = u_projection * u_view * u_model * vec4(position, 1.0);
 }
