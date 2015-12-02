849d812914fdc07ca40d426cdef6a2381af22a28
Bradley Crusco <bcrusco627@gmail.com>
2015-12-01 19:44:58 -0500

    New depth shaders that will only right depth information to a frame buffer and will not be used to do any ambient coloring.

---
+++ Forward-Plus/Forward-Plus/source/shaders/depth.vert.glsl
@@ -0,0 +1,11 @@
+#version 430
+
+layout (location = 0) in vec3 position;
+
+uniform mat4 u_projection;
+uniform mat4 u_view;
+uniform mat4 u_model;
+
+void main() {
+  gl_Position = u_projection * u_view * u_model * vec4(position, 1.0);
+}
