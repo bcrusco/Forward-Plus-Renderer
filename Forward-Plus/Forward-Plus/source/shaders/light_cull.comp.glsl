0e86d48878567f5105022409ec7b76d212fdd5c5
Bradley Crusco <bcrusco627@gmail.com>
2015-12-02 21:29:57 -0500

    More changes to the culling shader.

--- Forward-Plus/Forward-Plus/source/shaders/light_cull.comp.glsl
+++ Forward-Plus/Forward-Plus/source/shaders/light_cull.comp.glsl
@@ -2,12 +2,12 @@
 
 struct PointLight {
 	vec4 color;
-	vec3 position;
+	vec4 position;
 	float radius;
 };
 
 layout(std140, binding = 0) buffer LightBuffer {
-  PointLight data[];
+	PointLight data[];
 } lightBuffer;
 
 // This is a global that stores ALL the possible visible lights (1024 per tile)
@@ -18,15 +18,10 @@ layout(std140, binding = 1) buffer VisibleLightIndicesBuffer{
 
 // Uniforms
 uniform sampler2D u_depthTexture; // this should be global right?
-
-uniform int tileToLightMap[][];
-
-
 uniform mat4 view;
 uniform mat4 projection;
 uniform mat4 viewProjection;
 uniform vec2 screenSize;
-uniform float alpha;
 
 uniform int lightCount;
 
@@ -37,151 +32,120 @@ shared uint maxDepthInt;
 shared uint visibleLightCount;
 // For the light indices, is it a shared local thing for now and then at the end we write to the others?
 
+// Shared local storage for visible indices, will be written out to the buffer at the end
 shared uint visibleLightIndices[1024];
 
-shared uint tileLightStart;
-shared uint tileLightCount;
 shared vec4 frustumPlanes[6];
 
-layout (local_size_x = 16, local_size_y = 16) in;
+#define BLOCK_SIZE 16
+layout(local_size_x = BLOCK_SIZE, local_size_y = BLOCK_SIZE) in;
 void main() {
-  // Thread setup
-  ivec2 location = ivec2(gl_GlobalInvocationID.xy);
-  ivec2 itemID = ivec2(gl_LocalInvocationID.xy);
-  ivec2 tileID = ivec2(gl_WorkGroupID.xy);
-  ivec2 tileNumber = ivec2(gl_NumWorkGroups.xy);
-  int index = tileID.y * tileNumber.x + tileID.x;
+	// Thread setup
+	ivec2 location = ivec2(gl_GlobalInvocationID.xy);
+	ivec2 itemID = ivec2(gl_LocalInvocationID.xy);
+	ivec2 tileID = ivec2(gl_WorkGroupID.xy);
+	ivec2 tileNumber = ivec2(gl_NumWorkGroups.xy);
+	int index = tileID.y * tileNumber.x + tileID.x;
 
-  // initialize the shadered global values if we are the first thread
-  if(gl_LocalInvocationIndex == 0) {
-	  minDepthInt = 0xFFFFFFFF;
-	  maxDepthInt = 0;
-	  visibleLightCount = 0;
-  }
+	// initialize the shadered global values if we are the first thread
+	if(gl_LocalInvocationIndex == 0) {
+		minDepthInt = 0xFFFFFFFF;
+		maxDepthInt = 0;
+		visibleLightCount = 0;
+	}
 
-  // Sync threads
-  barrier();
+	// Sync threads
+	barrier();
 
+	float maxDepth, minDepth; // this should be in front of the barrier. Should it have some default value?
+	// step 1 is to calculate the min and max depth of this tile
+	vec2 text = vec2(location) / screenSize;
+	float depth = texture(u_depthTexture, text).r; // Is this line right? What is text doing? What is R?
+	// Wait this seems all messed up
+	uint depthInt = floatBitsToUint(depth);
+	atomicMax(maxDepthInt, depthInt);
+	atomicMin(minDepthInt, depthInt);
 
-  // step 1 is to calculate the min and max depth of this tile
-  vec2 text = vec2(location) / screenSize;
-  float depth = texture(u_depthTexture, text).r; // Is this line right? What is text doing?
-  atomicMax(tileMaxZ, floatBitsToUint(maxDepthInt));
-  atomicMin(tileMinZ, floatBitsToUint(minDepthInt));
+	// Sync threads
+	barrier();
 
+	// Step 2 is to then calculate the frustrum (only if we are the first thread)
+	
+	if(gl_LocalInvocationIndex == 0) {
+		maxDepth = uintBitsToFloat(maxDepthInt);
+		minDepth = uintBitsToFloat(minDepthInt);
 
-  // Sync threads
-  barrier();
+		vec2 negativeStep = (2.0 * vec2(tileID)) / vec2(tileNumber);
+		vec2 positiveStep = (2.0 * vec2(tileID + ivec2(1, 1))) / vec2(tileNumber);
 
-  // Step 2 is to then calculate the frustrum (only if we are the first thread)
-  if(gl_LocalInvocationIndex == 0) {
-    maxZ = uintBitsToFloat(maxDepthInt);
-	minZ = uintBitsToFloat(minDepthInt);
+		// Set up starting values for planes using steps and min and max z values
+		frustumPlanes[0] = vec4(1.0, 0.0, 0.0, 1.0 - negativeStep.x); // Left
+		frustumPlanes[1] = vec4(-1.0, 0.0, 0.0, -1.0 + positiveStep.x); // Right
+		frustumPlanes[2] = vec4(0.0, 1.0, 0.0, 1.0 - negativeStep.y); // Bottom
+		frustumPlanes[3] = vec4(0.0, -1.0, 0.0, -1.0 + positiveStep.y); // Top
+		frustumPlanes[4] = vec4(0.0, 0.0, -1.0, -minDepth); // Near
+		frustumPlanes[5] = vec4(0.0, 0.0, 1.0, maxDepth); // Far
 
-    vec2 negativeStep = (2.0 * vec2(tileID)) / vec2(tileNumber);
-    vec2 positiveStep = (2.0 * vec2(tileID + ivec2(1, 1))) / vec2(tileNumber);
+		// first four
+		for(uint i = 0; i < 4; i++) {
+			//frustumPlanes[i] *= viewProjection;
+			frustumPlanes[i] = frustumPlanes[i] * viewProjection;
+			frustumPlanes[i] /= length(frustumPlanes[i].xyz);
+		}
 
-    // Set up starting values for planes using steps and min and max z values
-    frustumPlanes[0] = vec4(1.0, 0.0, 0.0, 1.0 - negativeStep.x); // Left
-    frustumPlanes[1] = vec4(-1.0, 0.0, 0.0, -1.0 + positiveStep.x); // Right
-    frustumPlanes[2] = vec4(0.0, 1.0, 0.0, 1.0 - negativeStep.y); // Bottom
-    frustumPlanes[3] = vec4(0.0, -1.0, 0.0, -1.0 + positiveStep.y); // Top
-    frustumPlanes[4] = vec4(0.0, 0.0, -1.0, -minZ); // Near
-    frustumPlanes[5] = vec4(0.0, 0.0, 1.0, maxZ); // Far
+		// 5 and 6
+		frustumPlanes[4] = frustumPlanes[4] * view;
+		frustumPlanes[4] /= length(frustumPlanes[4].xyz);
+		frustumPlanes[5] = frustumPlanes[5] * view;
+		frustumPlanes[5] /= length(frustumPlanes[5].xyz);
+	}
 
-    // first four
-    for(uint i = 0; i < 4; i++) {
-      //frustumPlanes[i] *= viewProjection;
-      frustumPlanes[i] = frustumPlanes[i] * viewProjection;
-      frustumPlanes[i] /= length(frustumPlanes[i].xyz);
-    }
+	// Sync threads
+	barrier();
 
-    // 5 and 6
-    frustumPlanes[4] = frustumPlanes[4] * view;
-    frustumPlanes[4] /= length(frustumPlanes[4].xyz);
-    frustumPlanes[5] = frustumPlanes[5] * view;
-    frustumPlanes[5] /= length(frustumPlanes[5].xyz);
-  }
+	// cull lights as step 3
+	uint threadCount = BLOCK_SIZE * BLOCK_SIZE;
+	uint passCount = (lightCount + threadCount - 1) / threadCount;
+	// Now switch to the threads processing lights
+	for (uint i = 0; i < passCount; i++) {
+		uint lightIndex = i * threadCount + gl_LocalInvocationIndex;
 
-  // Sync threads
-  barrier();
+		// linear interpolation
+		// OK what is this?
+		// Why are we interpolating?
+		// Interpolating to smooth between light positions, maybe have to return to reactivate this later if we want animated lights
+		//vec4 position = mix(lightBuffer.data[i].previous, lightBuffer.data[i].current, alpha);
+		vec4 position = lightBuffer.data[i].position;
+		float radius = lightBuffer.data[i].radius;
 
-  // cull lights as step 3
-  vec4 position;
-  float radius;
+		// Check for intersections with every dimension of the frustrum
+		float distance = 0.0;
+		for(uint j = 0; j < 6; j++) {
+			distance = dot(position, frustumPlanes[j]) + radius;
 
-  uint previous;
-  uint next;
-  uint lightsPerTile = max(numberOfLights / 256, 1);
-  uint remainder = numberOfLights % 256;
-  uint lightStart;
-  uint lightEnd;
+			if(distance <= 0) {
+				break; // If one fails, then there is no intersection
+			}
+		}
 
-  // set up light start and end
-  if(gl_LocalInvocationIndex < remainder) {
-    lightsPerTile++;
-    lightStart = lightsPerTile * gl_LocalInvocationIndex;
-  }
-  else {
-    lightStart = remainder * (lightsPerTile + 1) + (gl_LocalInvocationIndex - remainder) * lightsPerTile;
-  }
+		// If greater than zero, then it is a visible light
+		if(distance > 0) {
+			// SO this increments it but returns the original so we know where WE are putting it, without telling the others
+			uint offset;
+			offset = atomicAdd(visibleLightCount, 1);
+			visibleLightIndices[offset] = lightIndex;
+		}
+	}
 
-  lightEnd = min(lightStart + lightsPerTile, numberOfLights);
-
-
-
-  
-
-
-  // Now switch to the threads processing lights
-  for(uint i = lightStart; i < lightEnd; i++) {
-
-	  uint lightIndex = i * threadCount + gl_LocalInvocationIndex;
-
-
-
-    // linear interpolation
-	  // OK what is this?
-	  // Why are we interpolating?
-    position = mix(lightBuffer.data[i].previous, lightBuffer.data[i].current, alpha);
-    radius = lightBuffer.data[i].velocityRadius.w;
-
-	// Check for intersections with every dimension of the frustrum
-    float distance = 0.0;
-    for(uint j = 0; j < 6; j++) {
-      distance = dot(position, frustumPlanes[j]) + radius;
-
-      if(distance <= 0) {
-        break; // If one fails, then there is no intersection
-      }
-    }
-
-	// If greater than zero, then it is a visible light
-    if(distance > 0) {
-      next = atomicCounterIncrement(nextInsertionPoint);
-      previous = atomicExchange(tileLightStart, next);
-
-      nodeBuffer.data[next].lightIndexAndNext = uvec4(i, previous, 0.0, 0.0);
-      
-
-
-
-		// SO this increments it but returns the original so we know where WE are putting it, without telling the others
-		uint offset;
-		offset = atomicAdd(tileLightCount, 1);
-		visibleLightIndices[offset] = lightIndex; //TODO: Where am I actualy storing this to?
-    }
-  }
-
-  // Sync threads
-  barrier();
+	// Sync threads
+	barrier();
 
 	if(gl_LocalInvocationIndex == 0) {
 		// One of the threads should write all the visible light indices to the proper buffer
 		uint offset = index * 1024;
 		// TODO: I should be able to just copy this in one call, look at later
 		for (uint i = 0; i < 1024; i++) {
-			visibleLightIndicies.data[offset + i] = visibleLightIndices[i];
+			visibleLightIndicesBuffer.data[offset + i] = visibleLightIndices[i];
 		}
 	}
 }
